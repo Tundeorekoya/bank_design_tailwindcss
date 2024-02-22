6091c002dad0200d3d057f3aac66b7ac5af41eb0
tundeorekoya <tundeorekoya1998@gmail.com>
2024-02-22 01:14:24 -0800

    update

--- src/components/Client.jsx
+++ src/components/Client.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import { clients } from "../constants"
+import styles from "../Style"
 
-const Client = () => {
-  return (
-    <div>
-      client
+
+const Client = () =>
+(
+  <section className={`${styles.flexCenter} my-4`} >
+
+    <div className={`${styles.flexCenter} flex-wrap w-full`}>
+      {
+        clients.map((client) => (
+          <div key={client.id} className={`${styles.flexCenter} flex-1 sm:min-w-[192px] min-w-[120px]`}>
+            <img src={client.logo} alt="logo" className={`sm:w-[192px] w-[100px]`} />
+          </div>
+        ))
+      }
     </div>
-  )
-}
+  </section>
+)
+
 
 export default Client
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -5,7 +5,7 @@ import { feedback } from "../constants"
 const Testimonials = () =>
 (
   <section id="clients" className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}>
-    <div />
+    <div className={`absolute w-[60%] h-[60%] rounded-full blue__gradient z-[0] -right-[50%]`}/>
 
     <div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
       <h1 className={styles.heading2}>
@@ -16,7 +16,7 @@ const Testimonials = () =>
       </div>
     </div>
 
-    <div className="flex flex-wrap sm:justify-start justify-center w-full feedback-container relative z-[1]">
+    <div className="flex flex-wrap sm:justify-start justify-center w-full feedback-container relative z-[1] ">
       {
         feedback.map((card, index) => (
           <FeedbackCard key={card.id} {...card} index={index} />
